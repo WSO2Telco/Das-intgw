a4c381a0a23cb33bf75ebc8f344eb2cad25d9d0c
Irham Iqbal <irham0019@gmail.com>
2017-02-01 14:42:30 +0530

    drop down fixes

--- features/hub-authentication-artifacts/hub-authentication-analytics/dashboard/HubAnalytics-Gadget-ResponseTimeReport_1.0.0/HubAnalytics-Gadget-ResponseTimeReport/js/core/gadget-core.js
+++ features/hub-authentication-artifacts/hub-authentication-analytics/dashboard/HubAnalytics-Gadget-ResponseTimeReport_1.0.0/HubAnalytics-Gadget-ResponseTimeReport/js/core/gadget-core.js
@@ -108,8 +108,7 @@ $(function () {
         // loadSP();
         // loadApp();
         // loadApi();
-
-        function loadOperator (){
+      function loadOperator (){
             conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_OPERATOR_SUMMARY";
             conf["provider-conf"]["provider-name"] = "operator";
             conf.operator = -1;
@@ -131,7 +130,6 @@ $(function () {
                         operatorsItems += '<li><a data-val='+ operator.operatorId +' href="#">' + operator.operatorName +'</a></li>';
                         operatorIds.push(" "+operator.operatorId);
                     }
-
                     $("#dropdown-operator").html( $("#dropdown-operator").html() + operatorsItems);
                     $("#button-operator").val('<li><a data-val="-1" href="#">All</a></li>');
                     loadSP(operatorIds);
@@ -145,9 +143,9 @@ $(function () {
                     });
                 }
             });
-        }
+          }
 
-        function loadSP (clickedOperator){
+          function loadSP (clickedOperator){
 
             conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_API_SUMMARY";
             conf["provider-conf"]["provider-name"] = "operator";
@@ -164,12 +162,16 @@ $(function () {
                     $("#dropdown-sp").empty();
                     var spItems = '';
                     var spIds = [];
+                    var loadedSps = [];
                     spIds.push(serviceProviderId);
                     spItems += '<li><a data-val="-1" href="#">All</a></li>';
                     for ( var i =0 ; i < data.length; i++) {
                         var sp = data[i];
+                        if($.inArray(sp.serviceProviderId, loadedSps)<0){
                         spItems += '<li><a data-val='+ sp.serviceProviderId +' href="#">' + sp.serviceProvider +'</a></li>'
                         spIds.push(" "+sp.serviceProviderId);
+                        loadedSps.push(sp.serviceProviderId);
+                      }
                     }
 
                     $("#dropdown-sp").html(spItems);
@@ -195,96 +197,96 @@ $(function () {
         }
 
         function loadApp (sps){
-            // alert(sps);
-            // if(sps)
-            conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_API_SUMMARY";
-            conf["provider-conf"]["provider-name"] = "sp";
-            applicationId = -1;
-            conf.serviceProvider = "("+sps+")";
-            $.ajax({
-                url: gadgetLocation + '/gadget-controller.jag?action=getData',
-                method: "POST",
-                data: JSON.stringify(conf),
-                contentType: "application/json",
-                async: false,
-                success: function (data) {
+        // alert(sps);
+        // if(sps)
+        conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_API_SUMMARY";
+        conf["provider-conf"]["provider-name"] = "sp";
+        applicationId = -1;
+        conf.serviceProvider = "("+sps+")";
+        $.ajax({
+            url: gadgetLocation + '/gadget-controller.jag?action=getData',
+            method: "POST",
+            data: JSON.stringify(conf),
+            contentType: "application/json",
+            async: false,
+            success: function (data) {
 
+                $("#dropdown-app").empty();
+                var apps = [];
+                var loadedApps = [];
+                var appItems = '<li><a data-val="-1" href="#">All</a></li>';
+                for ( var i =0 ; i < data.length; i++) {
+                    var app = data[i];
+                    if($.inArray(app.applicationId, loadedApps)<0){
+                    appItems += '<li><a data-val='+ app.applicationId +' href="#">' + app.applicationName +'</a></li>'
+                    apps.push(" "+app.applicationId);
+                    loadedApps.push(app.applicationId);
+                  }
+                }
 
-                    $("#dropdown-app").empty();
-                    var apps = [];
-                    var appItems = '<li><a data-val="-1" href="#">All</a></li>';
-                    for ( var i =0 ; i < data.length; i++) {
-                        var app = data[i];
-                        appItems += '<li><a data-val='+ app.applicationId +' href="#">' + app.applicationName +'</a></li>'
-                        apps.push(" "+app.applicationId);
-                    }
+                $("#dropdown-app").html( $("#dropdown-app").html() + appItems);
+                $("#button-app").val('<li><a data-val="-1" href="#">All</a></li>');
+                $("#button-app").text('All');
+                // loadApp(sps[i]);
 
-                    $("#dropdown-app").html( $("#dropdown-app").html() + appItems);
-                    $("#button-app").val('<li><a data-val="-1" href="#">All</a></li>');
-                    $("#button-app").text('All');
-                    // loadApp(sps[i]);
+                loadApi(apps);
 
+                $("#dropdown-app li a").click(function(){
+
+                    $("#button-app").text($(this).text());
+                    $("#button-app").append('<span class="caret"></span>');
+                    $("#button-app").val($(this).text());
+                    // var clickedSP = [];
+                    // clickedSP.push($(this).data('val'));
+                    apps = $(this).data('val');
+                    applicationId = apps;
                     loadApi(apps);
-                    $("#dropdown-app li a").click(function(){
+                });
 
-                        $("#button-app").text($(this).text());
-                        $("#button-app").append('<span class="caret"></span>');
-                        $("#button-app").val($(this).text());
-                        // var clickedSP = [];
-                        // clickedSP.push($(this).data('val'));
-                        apps = $(this).data('val');
-                        applicationId = apps;
-                        loadApi(apps);
-                    });
-
-                }
-            });
-        }
-
-        function loadApi (apps){
-            conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_API_SUMMARY";
-            conf["provider-conf"]["provider-name"] = "app";
-
-            if(apps == ""){
-                conf.applicationId = "('"+apps+"')";
-            } else {
-                conf.applicationId = "("+apps+")";
             }
-            apiId = -1;
+        });
+      }
 
-            $.ajax({
-                url: gadgetLocation + '/gadget-controller.jag?action=getData',
-                method: "POST",
-                data: JSON.stringify(conf),
-                contentType: "application/json",
-                async: false,
-                success: function (data) {
-                    // alert("loadApi :" +JSON.stringify(data));
+      function loadApi (apps){
+      conf["provider-conf"]["tableName"] = "ORG_WSO2TELCO_ANALYTICS_HUB_STREAM_API_SUMMARY";
+      conf["provider-conf"]["provider-name"] = "app";
+      conf.applicationId = "("+apps+")";;
+      apiId = -1;
+      $.ajax({
+          url: gadgetLocation + '/gadget-controller.jag?action=getData',
+          method: "POST",
+          data: JSON.stringify(conf),
+          contentType: "application/json",
+          async: false,
+          success: function (data) {
+            // alert("loadApi :" +JSON.stringify(data));
 
-                    $("#dropdown-api").empty();
-                    var apis = [];
-                    var apiItems = '<li><a data-val="-1" href="#">All</a></li>';
-                    for ( var i =0 ; i < data.length; i++) {
-                        var api = data[i];
-                        apiItems += '<li><a data-val='+ api.apiID +' href="#">' + api.api +'</a></li>'
-                    }
-
-                    $("#dropdown-api").html( $("#dropdown-api").html() + apiItems);
-                    $("#button-api").val('<li><a data-val="-1" href="#">All</a></li>');
-                    $("#button-api").text('All');
-                    // loadApp(sps[i]);
-                    $("#dropdown-api li a").click(function(){
-                        $("#button-api").text($(this).text());
-                        $("#button-api").append('<span class="caret"></span>');
-                        $("#button-api").val($(this).text());
-                        apiId = $(this).data('val');
-                    });
-
+              $("#dropdown-api").empty();
+              var apis = [];
+              var loadedApis = [];
+              var apiItems = '<li><a data-val="-1" href="#">All</a></li>';
+              for ( var i =0 ; i < data.length; i++) {
+                  var api = data[i];
+                  if($.inArray(api.apiID, loadedApis)<0){
+                  apiItems += '<li><a data-val='+ api.apiID +' href="#">' + api.api +'</a></li>';
+                  loadedApis.push(api.apiID);
                 }
-            });
-        }
+              }
 
+              $("#dropdown-api").html( $("#dropdown-api").html() + apiItems);
+              $("#button-api").val('<li><a data-val="-1" href="#">All</a></li>');
+              $("#button-api").text('All');
+              // loadApp(sps[i]);
+              $("#dropdown-api li a").click(function(){
+                  $("#button-api").text($(this).text());
+                  $("#button-api").append('<span class="caret"></span>');
+                  $("#button-api").val($(this).text());
+                  apiId = $(this).data('val');
+              });
 
+          }
+      });
+    }
 
         $("#button-app").val("All");
         $("#button-api").val("All");
